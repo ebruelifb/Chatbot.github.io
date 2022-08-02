4edfe9865dfff7d366d5e0fdc4c609ecdc39e237
ebruelifb <81680247+ebruelifb@users.noreply.github.com>
2022-08-02 21:50:05 +0200

    V2

--- script/main.js
+++ script/main.js
@@ -1,12 +1,14 @@
 
-window.watsonAssistantChatOptions = {
-    integrationID: "2fdf0ad6-3aa2-4f68-b03a-422cf48226bf", // The ID of this integration.
+
+  window.watsonAssistantChatOptions = {
+    integrationID: "8d8183e0-fde1-494f-b25d-d21042243783", // The ID of this integration.
     region: "au-syd", // The region your integration is hosted in.
     serviceInstanceID: "9d341ccd-423a-47a3-9e85-524d04bf5e5f", // The ID of your service instance.
     onLoad: function(instance) { instance.render(); }
-};
-setTimeout(function(){
+  };
+  setTimeout(function(){
     const t=document.createElement('script');
     t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
     document.head.appendChild(t);
-});
+  });
+
